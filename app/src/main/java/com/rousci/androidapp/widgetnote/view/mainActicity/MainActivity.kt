a849193c2cbc326c65304bc7938273b72c5813f8
rousci <yuanke2016@gmail.com>
2017-10-13 00:34:58 +0800

    add basic toolbar

--- app/src/main/java/com/rousci/androidapp/widgetnote/view/mainActicity/MainActivity.kt
+++ app/src/main/java/com/rousci/androidapp/widgetnote/view/mainActicity/MainActivity.kt
@@ -5,20 +5,18 @@ import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
-import android.util.Log
-import android.widget.Button
 import com.rousci.androidapp.widgetnote.R
-import com.rousci.androidapp.widgetnote.model.insert
 import com.rousci.androidapp.widgetnote.model.queryAll
 import com.rousci.androidapp.widgetnote.model.setDatabase
 import org.jetbrains.anko.find
-import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(find(R.id.toolbar))
 
         setDatabase(applicationContext)
         val dataSet = queryAll()
@@ -27,9 +25,6 @@ class MainActivity : AppCompatActivity() {
         layoutManager.orientation = LinearLayoutManager.VERTICAL
         recycleView.layoutManager = layoutManager
         recycleView.adapter = StringRecycleAdapter(dataSet, this)
-
-        val toolbar = find<Toolbar>(R.id.toolbar)
-
     }
 
 }
