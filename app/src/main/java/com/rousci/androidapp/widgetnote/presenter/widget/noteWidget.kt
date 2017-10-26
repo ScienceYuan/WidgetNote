44437b12f5c0f71c60eb00194d75a36f847f827d
rousci <yuanke2016@gmail.com>
2017-10-26 16:58:29 +0800

    update a better algorithm

--- app/src/main/java/com/rousci/androidapp/widgetnote/presenter/widget/noteWidget.kt
+++ app/src/main/java/com/rousci/androidapp/widgetnote/presenter/widget/noteWidget.kt
@@ -2,6 +2,7 @@ package com.rousci.androidapp.widgetnote.presenter.widget
 
 import android.appwidget.AppWidgetManager
 import android.content.Context
+import android.util.Log
 import android.widget.RemoteViews
 import com.rousci.androidapp.widgetnote.R
 import com.rousci.androidapp.widgetnote.model.lastNoteIdPre
@@ -19,19 +20,31 @@ fun updateAppWidget(context: Context?, appWidgetManager: AppWidgetManager?, appW
 
     setDatabase(context!!)
     val lastNoteId = context!!.getSharedPreferences(lastNoteIdPre, 0).getInt("lastNoteId", -1)
-    val notes = queryAll().filterIndexed { index, s -> index != lastNoteId }
+    val notes = queryAll()
     val lastIndex = notes.lastIndex
 
     if(lastIndex != -1){
 
         val views = RemoteViews(context!!.packageName, R.layout.note_widget)
-        val random = Math.abs(Random().nextInt() % (lastIndex + 1))
 
-        context!!.getSharedPreferences(lastNoteIdPre, 0).edit().putInt("lastNoteId", random)
+        val randomIndex = fun ():Int{
+            val random = Math.abs(Random().nextInt() % (lastIndex + 1))
+            return if (random == lastNoteId){
+                val randomAdd = Random().nextInt(random)
+                (random + randomAdd) % (lastIndex + 1)
+            }
+                else{
+                random
+            }
+        }()
+
+        val editor = context!!.getSharedPreferences(lastNoteIdPre, 0).edit()
+        editor.putInt("lastNoteId", randomIndex)
+        editor.commit()
 
         for (id in appWidgetIds!!){
 
-            views.setTextViewText(R.id.appwidget_text, notes[random])
+            views.setTextViewText(R.id.appwidget_text, notes[randomIndex])
             // Instruct the widget manager to update the widget
             appWidgetManager!!.updateAppWidget(id, views)
         }
