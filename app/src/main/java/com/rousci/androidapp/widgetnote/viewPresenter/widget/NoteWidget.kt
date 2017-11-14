27d4da26629c17c815b478fe124e87c52f59d45f
rousci <yuanke2016@gmail.com>
2017-11-14 16:04:07 +0800

    handle an expection

--- app/src/main/java/com/rousci/androidapp/widgetnote/viewPresenter/widget/NoteWidget.kt
+++ app/src/main/java/com/rousci/androidapp/widgetnote/viewPresenter/widget/NoteWidget.kt
@@ -3,6 +3,8 @@ package com.rousci.androidapp.widgetnote.viewPresenter.widget
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.widget.Toast
+import com.rousci.androidapp.widgetnote.R
 import com.rousci.androidapp.widgetnote.model.queryAll
 import com.rousci.androidapp.widgetnote.model.setDatabase
 
@@ -13,7 +15,12 @@ class NoteWidget : AppWidgetProvider() {
 
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         setDatabase(context)
-        updateWidgetOnTime(context, appWidgetManager, appWidgetIds, queryAll())
+        try {
+            updateWidgetOnTime(context, appWidgetManager, appWidgetIds, queryAll())
+        }
+        catch (e: ArithmeticException){
+            Toast.makeText(context, R.string.empty_data, Toast.LENGTH_SHORT).show()
+        }
         super.onUpdate(context, appWidgetManager, appWidgetIds)
     }
 
