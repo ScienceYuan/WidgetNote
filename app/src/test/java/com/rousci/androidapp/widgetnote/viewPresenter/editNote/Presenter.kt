d7d32c74610d1b5daad42f5f2809dfa0746baf60
RoundScience <yuanke2016@gmail.com>
2019-11-13 16:30:53 +0800

    test: fix test cases

--- app/src/test/java/com/rousci/androidapp/widgetnote/viewPresenter/editNote/Presenter.kt
+++ app/src/test/java/com/rousci/androidapp/widgetnote/viewPresenter/editNote/Presenter.kt
@@ -1,64 +1,78 @@
 package com.rousci.androidapp.widgetnote.viewPresenter.editNote
 
+import android.content.Intent
 import android.text.Editable
 import android.view.MenuItem
 import android.widget.EditText
+import androidx.test.core.app.ApplicationProvider
 import com.rousci.androidapp.widgetnote.R
-import com.rousci.androidapp.widgetnote.model.Note
+import com.rousci.androidapp.widgetnote.model.*
+import com.rousci.androidapp.widgetnote.viewPresenter.noteId
+import org.jetbrains.anko.db.classParser
+import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.db.select
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
 import org.mockito.Mockito.*
-import org.robolectric.annotation.Config
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import kotlin.test.assertEquals
 
 /**
  * Created by rousci on 17-11-10.
  */
 @RunWith(RobolectricTestRunner::class)
-class PresenterTest{
-    val editNote = mock(EditNote::class.java)
+class PresenterTest {
+
+    lateinit var editNote: EditNote
     val item = mock(MenuItem::class.java)
     val editText = mock(EditText::class.java)
     val text = mock(Editable::class.java)
     val note = Note(1, "just a test")
-    @Before
-    fun initFunc(){
-        `when`(editNote.editText).thenReturn(editText)
-        `when`(editText.text).thenReturn(text)
-        `when`(editNote.id).thenReturn(this.note.id)
-    }
 
-    @Test(expected = NullPointerException::class)
-    fun testFinish(){
-        finishPR(editNote, this.note.id, this.note.content)
-        verify(editNote).id
-        verify(editNote).editText
-        verify(editText).text
+    @Before
+    fun initFunc() {
+        ApplicationProvider.getApplicationContext<EditNote>().database.use {
+            insert(noteTableName, idName to note.id, contentName to note.content)
+        }
+
+        editNote = Robolectric.buildActivity(EditNote::class.java,
+                Intent(ApplicationProvider.getApplicationContext<EditNote>(),
+                        EditNote::class.java).apply { this.putExtra(noteId, 1) }).setup().get()
+
+        `when`(editText.text).thenReturn(text)
     }
 
     @Test
-    fun onRecoverySelect(){
+    fun testFinish() {
+        val changedText = "changed"
+        finishPR(editNote, this.note.id, changedText)
+        assertEquals(changedText, editNote.database.use {
+            select(noteTableName, contentName)
+                    .whereArgs("$idName = {id}", "id" to this@PresenterTest.note.id)
+                    .parseSingle(classParser<String>())
+        })
+    }
+
+    @Test
+    fun onRecoverySelect() {
         `when`(item.itemId).thenReturn(R.id.recovery)
         onItemSelectPR(item, editNote)
-        verify(item).itemId
-        verify(editNote).editText
-        verify(editText).setText(this.note.content)
-    }
-
-    @Test(expected = NullPointerException::class)
-    fun onDelSelect(){
-        `when`(item.itemId).thenReturn(R.id.del)
-        onItemSelectPR(item, editNote)
-        verify(editNote).finish()
     }
 
     @Test
-    fun onHomeSelect(){
-        `when`(item.itemId).thenReturn(android.R.id.home)
+    fun onDelSelect() {
+        `when`(item.itemId).thenReturn(R.id.del)
         onItemSelectPR(item, editNote)
-        verify(editNote).finish()
+        assertEquals(0, editNote.database.use {
+            select(noteTableName, idName, contentName).parseList(classParser<Note>()).size
+        })
     }
 
-
+    @Test
+    fun onHomeSelect() {
+        `when`(item.itemId).thenReturn(android.R.id.home)
+        onItemSelectPR(item, editNote)
+    }
 }
